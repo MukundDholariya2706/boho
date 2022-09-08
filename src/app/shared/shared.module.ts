05996afeb91d6cf0e032a5f3d6f30135d8441117
MukundDholariya2706 <mukunddtridhyatech@gmail.com>
2022-09-08 18:22:01 +0530

    add navbar

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenubarModule } from 'primeng/menubar'
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+  ]
+})
+export class SharedModule { }
