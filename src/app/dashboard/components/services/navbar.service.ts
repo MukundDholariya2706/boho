c02c2209fdfc17bf864b3c5c23bd2249faffb6e8
MukundDholariya2706 <mukunddtridhyatech@gmail.com>
2022-09-12 14:59:52 +0530

    custom navigation add

--- src/app/dashboard/components/services/navbar.service.ts
+++ src/app/dashboard/components/services/navbar.service.ts
@@ -11,6 +11,7 @@ export class NavbarService {
     {
       link: "",
       title: "Design Ideas",
+      position: "left",
       subMenus: [
         {
           link: "",
@@ -72,11 +73,13 @@ export class NavbarService {
     },
     {
       link: "",
-      title: "How it works"
+      title: "How it works",
+      position: "left",
     },
     {
       link: "",
       title: "Offerings",
+      position: "left",
       subDivision: [
 
       ]
@@ -84,6 +87,7 @@ export class NavbarService {
     {
       link: "",
       title: "Cities",
+      position: "left",
       subMenus: [
         {
           link: "",
@@ -108,8 +112,132 @@ export class NavbarService {
         {
           link: "",
           title: "Goa"
+        },
+        {
+          link: "",
+          title: "Rajkot"
         }
       ]
+    },
+    {
+      link: "",
+      title: "Livspace Homes",
+      position: "left",
+    },
+    {
+      link: "",
+      title: "More",
+      position: "left",
+      subMenus: [
+        {
+          link: "",
+          title: "The Modular Journey"
+        },
+        {
+          link: "",
+          title: "Refer a Friend"
+        },
+        {
+          link: "",
+          title: "Livspace for Business"
+        },
+        {
+          link: "",
+          title: "Press"
+        },
+        {
+          link: "",
+          title: "Careers"
+        },
+        {
+          link: "",
+          title: "Partner with us"
+        },
+        {
+          link: "",
+          title: "Visit Us"
+        },
+        {
+          link: "",
+          title: "About Us"
+        },
+        {
+          link: "",
+          title: "Contact Us"
+        },
+        {
+          link: "",
+          title: "Policies"
+        }
+      ]
+    },
+    {
+      link: "",
+      title: "Magazine",
+      position: "right",
+      subMenus: [
+        {
+          link: "",
+          title: "Room ideas"
+        },
+        {
+          link: "",
+          title: "Decor & Inspiration"
+        },
+        {
+          link: "",
+          title: "Ceiling Design"
+        },
+        {
+          link: "",
+          title: "Furniture ideas"
+        },
+        {
+          link: "",
+          title: "Home Decor"
+        },
+        {
+          link: "",
+          title: "Lighting Ideas"
+        },
+        {
+          link: "",
+          title: "Wall Design Ideas"
+        },
+        {
+          link: "",
+          title: "Expert Advice"
+        },
+        {
+          link: "",
+          title: "Interior Advice"
+        },
+        {
+          link: "",
+          title: "Vastu Tips"
+        },
+        {
+          link: "",
+          title: "Home Organisation"
+        },
+        {
+          link: "",
+          title: "Home Organisation"
+        },
+        {
+          link: "",
+          title: "Materisal Guids"
+        },
+        {
+          link: "",
+          title: "Home Renovation Ideas"
+        }
+      ]
+    },
+    {
+      link: "",
+      title: "Signin / Signup",
+      position: "right"
     }
   ];
 
